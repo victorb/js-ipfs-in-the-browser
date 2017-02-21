95d8b08750290c4564b649741d290b956de20479
Victor Bjelkholm <victorbjelkholm@gmail.com>
2017-02-21 16:40:58 +0100

    Cleanup

--- page-script.js
+++ page-script.js
@@ -1,12 +1,10 @@
 /* global browser, cloneInto */
-// window.wrappedJSObject.ipfs = false
 console.log('declared a public ipfs object!')
 
 const myPort = browser.runtime.connect({name: 'port-from-cs'})
 
 const makeCall = (method, cb) => {
   myPort.onMessage.addListener(function (m) {
-    // myPort.onMessage.removeListener(this)
     cb(cloneInto(m, window, {cloneFunctions: true}))
   })
   myPort.postMessage({method})
@@ -16,15 +14,7 @@ const ipfs = {
   id: (callback) => { makeCall('id', callback) }
 }
 
-// ipfs.id((id) => {
-//   console.log('lol', id)
-// })
-
 window.wrappedJSObject.ipfs = cloneInto(
   ipfs,
   window,
   {cloneFunctions: true})
-
-// window.ipfs = ipfs
-// unsafeWindow.clonedContentScriptObject = cloneInto(ipfs, unsafeWindow)
-// unsafeWindow.assignedContentScriptObject = ipfs;
--- readme.md
+++ readme.md
@@ -1,1 +1,19 @@
 ## js-ipfs in the browser
+
+### Running locally
+
+* `npm install`
+* `npm run build`
+* Open `about:debugging` in Firefox and point it to the manifest.json in this repository
+* Now try the test page: https://ipfs.io/ipfs/QmcVc8eQiWkR23wMKiQjipLRSjCxR1FEj4TL99aY89J83d
+
+### Packaging
+
+* Sign up for AMO https://addons.mozilla.org/
+* Run `npm run package`
+* Upload the resulting zip to AMO
+* You'll get back an .xpi you can use for installing the extension
+
+## License
+
+MIT 2017 - Victor Bjelkholm
